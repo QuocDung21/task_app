3b4d3d0c975205180cfddd339dde00b0ccf0d931
QuocDung21 <rongvang2357hn@gmail.com>
2023-09-22 06:35:59 +0700

    add new login

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,5 +1,4 @@
-import { useSession, signIn, signOut } from 'next-auth/react'
-import Image from 'next/image'
+import { useSession } from 'next-auth/react'
 
 const info = () => {
     const { data: session } = useSession()
@@ -8,9 +7,9 @@ const info = () => {
             {
                 session ? (<>
                     <div className='flex justify-center items-center'>
-                        <div className='mr-2'> Xin chào : {session.user?.name}</div>
+                        <div className='mr-2'> Xin chào : {session?.user?.name}</div>
                         <div>
-                             {/* <Image src={session?.user?.image} className='rounded-xl' width={30} alt="Picture of the author" height={30} /> */}
+                            {/* <Image src={session?.user?.image} className='rounded-xl' width={30} alt="Picture of the author" height={30} /> */}
                         </div>
                     </div>
                 </>) : " Ghi chú của tôi"
