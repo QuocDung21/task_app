9b7610849d775e8ff49025e35323bf961aafa4e1
QuocDung21 <rongvang2357hn@gmail.com>
2023-09-22 06:30:36 +0700

    add login

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -10,7 +10,7 @@ const info = () => {
                     <div className='flex justify-center items-center'>
                         <div className='mr-2'> Xin chào : {session.user?.name}</div>
                         <div>
-                            <Image src={session?.user?.image} className='rounded-xl' width={30} alt="Picture of the author" height={30} />
+                            {/* <Image src={session?.user?.image} className='rounded-xl' width={30} alt="Picture of the author" height={30} /> */}
                         </div>
                     </div>
                 </>) : " Ghi chú của tôi"
